e60bcff65754bfc62ea62a09ddd18ec9c243ed53
Noflowerzzk <Noflowerzzk@outlook.com>
2024-11-17 18:14:25 +0800

    凸函数定义完成

--- 凸函数/凸函数.py
+++ 凸函数/凸函数.py
@@ -4,6 +4,7 @@ from manim import *
 from manim._config import *
 
 config["tex_template"] = "ctex"
+# config.quality = "low_quality"
 
 class ConvexFunction(Scene):
     
@@ -12,32 +13,26 @@ class ConvexFunction(Scene):
         # 片头
         self.MyLogo()
         
-        self.DefineConvexFunction_1()
+        self.DefineConvexFunction()
         
-    def DefineConvexFunction_1(self):
+    def DefineConvexFunction(self):
         DefOfConvexFunction = MathTex(
             r"& \text{对于函数 }\ f(x) \text{ 定义域内任意两数 } x_1 < x_2, \text{ 有任意 } \lambda \in [0, 1], \text{ 满足} \\",
             r"& f(\lambda x_1 + (1 - \lambda) x_2) \leq \lambda f(x_1) + (1 - \lambda) f(x_2) \\",
             r"& \text{则 }\ f(x) \text{ 为其定义区间上的下凸函数.}",
-            tex_environment="align*"            
+            tex_environment="align*"
         )
         
         DefOfConvexFunction.shift(UP * 2 + LEFT * 2).font_size = 30
         
         self.play(Write(DefOfConvexFunction))
+
+        # self.DefineConvexFunctionGraph()
         
-        self.DefineConvexFunctionGraph()
-        
-        # 淡出所有对象
-        self.play(*[FadeOut(obj) for obj in self.mobjects])
-        
-        self.wait(3)
-        
-    def DefineConvexFunctionGraph(self):
-        # 创建一个小型坐标轴
+                # 创建一个小型坐标轴
         axes = Axes(
-            x_range=[-0.1, 3, 0.5],  # 设置 x 轴范围 [-2, 2]，步长为 1
-            y_range=[-0.1, 2, 0.5],  # 设置 y 轴范围 [-1, 1]，步长为 0.5
+            x_range=[-0.1, 3, 0.5],
+            y_range=[-0.1, 2, 0.5],
             y_length=4.2,
             x_length=6.2,
             axis_config={"color": BLUE},  # 坐标轴颜色
@@ -60,12 +55,14 @@ class ConvexFunction(Scene):
             axes.c2p(0, ConvexFunc(0)),
             color=GREEN
         )
+        LabelA = Tex("A", font_size=30).next_to(dotA, direction=LEFT)
         dotB = Dot(
             axes.c2p(2.25, ConvexFunc(2.25)),
             color=GREEN
         )
+        LabelB = Tex("B", font_size=30).next_to(dotB, direction=RIGHT)
         
-        line = Line(
+        LineAB = Line(
             start=dotA.get_center(),
             end=dotB.get_center(),
             color=GREEN
@@ -77,7 +74,7 @@ class ConvexFunction(Scene):
 
         MovingDotOnLine = always_redraw(
             lambda: Dot(
-                line.point_from_proportion(t.get_value()),
+                LineAB.point_from_proportion(t.get_value()),
                 color=MovingDotColor
             )
         )
@@ -87,6 +84,9 @@ class ConvexFunction(Scene):
                 axes.c2p(t.get_value() * 2.25, ConvexFunc(t.get_value() * 2.25)),
                 color=MovingDotColor
             )
+        )
+        LabelT = always_redraw(
+            lambda: Tex("T", font_size=30).next_to(MovingDotOnCurve, DOWN)
         )
         
         VerticalLine = always_redraw(
@@ -116,21 +116,153 @@ class ConvexFunction(Scene):
             Create(axes), Write(labels),
             Create(ConvexFuncGraph), 
             Create(dotA), Create(dotB),
+            Write(LabelA), Write(LabelB)
         )
-        self.play(Create(line), run_time=0.7)
+        self.play(Create(LineAB), run_time=0.7)
         self.play(
             FadeIn(MovingDotOnLine), FadeIn(MovingDotOnCurve),
             Create(VerticalLine),
             Write(LambdaInGraph), Write(NumberInGraph),
+            Write(LabelT),
             run_time=0.5
         )
         self.play(t.animate.set_value(1))
         self.play(t.animate.set_value(0))
         self.play(t.animate.set_value(0.4))
+                
+        # 另一个定义
         
-        self.wait(2)
+        PropertyOfConvexFunction = Tex(
+            r"& \text{连接}\ \mathrm{AT}, \  \mathrm{BT}, \\",
+            r"& \text{不难发现, 在}\lambda \in (0, 1) \text{时}, \\",
+            r"& k_{\mathrm{AT}} \leq k_{\mathrm{AB}} \leq k_{\mathrm{BT}}",
+            tex_environment="align*",
+            font_size=30
+        ).next_to(DefOfConvexFunction, direction=DOWN, buff=0.5).to_edge(LEFT)
         
-    # def DefineConvexFunction_2(self):
+        PropertyOfConvexFunction.color = YELLOW
+        
+        LineAT = always_redraw(
+            lambda: Line(
+                start=dotA, end=MovingDotOnCurve,
+                color=GREEN
+            )
+        )
+        LineBT = always_redraw(
+            lambda: Line(
+                start=dotB, end=MovingDotOnCurve,
+                color=GREEN
+            )
+        )
+        
+        # 描述斜率关系
+        ChangingLine = LineAT.copy()
+
+
+        self.play(
+            Write(PropertyOfConvexFunction),
+            Create(LineAT, run_time=0.5), Create(LineBT, run_time=0.5),
+        )
+        
+        kAT = PropertyOfConvexFunction[2][0].copy()
+        kAB = PropertyOfConvexFunction[2][2].copy()
+        kBT = PropertyOfConvexFunction[2][4].copy()
+        
+        self.play(Transform(kAT, LineAT), run_time=1)
+        self.play(Transform(ChangingLine, LineAB), Transform(kAB, LineAB, run_time=1))
+        ChangingLine.put_start_and_end_on(ChangingLine.get_end(), ChangingLine.get_start())
+        self.play(Transform(ChangingLine, LineBT), Transform(kBT, LineBT, run_time=1))
+        self.remove(ChangingLine, kAT, kAB, kBT)
+        
+        self.play(t.animate.set_value(0.8))
+        self.play(t.animate.set_value(0.2))
+        self.play(t.animate.set_value(0.6))
+        
+        BringUpAnotherDef = MathTex(
+            r"& \text{所以我们有凸函数的性质：}",
+            tex_environment="align*",
+            color=YELLOW,
+            font_size=48
+        ).to_corner(UL).shift(DOWN * 0.5)
+        
+        AnotherDef = MathTex(
+            r"& f(x) \text{ 是 } (a, b) \text{ 上的下凸函数当且仅当任意 } (x_1, x_2) \subseteq (a, b),\\",
+            r"& \text{ 任意 } x \in (x_1, x_2), \text{ 有} \\",
+            r"& \frac{f(x) - f(x_1)}{x - x_1} \leqslant \frac{f(x_2) - f(x_1)}{x_2 - x_1} \leqslant \frac{f(x_2) - f(x)}{x_2 - x}",
+            font_size=30
+        ).align_to(DefOfConvexFunction.get_left(), LEFT).shift(UP * 1.2)
+        
+        AnotherDef[2].set_opacity(0)
+        
+        kAT = AnotherDef[2][0 : 15].copy().set_opacity(1)
+        kAB = AnotherDef[2][16 : 33].copy().set_opacity(1)
+        kBT = AnotherDef[2][34 : ].copy().set_opacity(1)
+        Leq1 = AnotherDef[2][15].copy().set_opacity(1)
+        Leq2 = AnotherDef[2][33].copy().set_opacity(1)
+        LineAT_cpy = LineAT.copy()
+        LineAB_cpy = LineAB.copy()
+        LineBT_cpy = LineBT.copy()
+        
+        self.play(
+            Transform(DefOfConvexFunction, AnotherDef),
+            Transform(PropertyOfConvexFunction, BringUpAnotherDef)
+        )
+        
+        self.play(Transform(LineAT_cpy, kAT))
+        self.play(Transform(LineAB_cpy, kAB), Write(Leq1))
+        self.play(Transform(LineBT_cpy, kBT), Write(Leq2))
+        
+        self.play(DefOfConvexFunction[2].animate.set_opacity(1))
+        self.remove(kAT, kAB, kBT, Leq1, Leq2, LineAT_cpy, LineAB_cpy, LineBT_cpy)
+                
+        self.play(
+            FadeOut(PropertyOfConvexFunction),
+            DefOfConvexFunction.animate.shift(UP * 1),
+            # 清除图形
+            FadeOut(axes, labels, LabelA, LabelB, LabelT, dotA, dotB, LambdaInGraph, NumberInGraph, VerticalLine, LineAT, LineBT, LineAB, MovingDotOnCurve, MovingDotOnLine, ConvexFuncGraph)
+        )
+        
+        Proof = MathTex(
+            r"& \text{证明}: \\",
+            r"& \text{由} x = \frac{x_2 - x}{x_2 - x_1}x_1 + \frac{x - x_1}{x_2 - x_1}x_2, \text{有} \\",
+            r"& f(x) \text{是下凸函数} \Leftrightarrow  f(x) \leqslant \frac{x_2 - x}{x_2 - x_1}f(x_1) + \frac{x - x_1}{x_2 - x_1}f(x_2)\\",
+            font_size=27
+        ).next_to(DefOfConvexFunction, direction=DOWN).align_to(DefOfConvexFunction.get_left(), direction=LEFT)
+        
+        self.play(Write(Proof))
+        
+        TempInProofOri = MathTex(
+            r"& \Leftrightarrow\quad  \frac{f(x) - f(x_1)}{x - x_1} \leqslant \frac{f(x_2) - f(x_1)}{x_2 - x_1}",
+            font_size=27
+        ).next_to(Proof, DOWN).align_to(Proof.get_left(), LEFT)
+        
+        TempInProofTrans_1 = MathTex(
+            r"& \Leftarrow\quad  \frac{\frac{x_2 - x}{x_2 - x_1}f(x_1) + \frac{x - x_1}{x_2 - x_1}f(x_2) - f(x_1)}{x - x_1} \leqslant \frac{f(x_2) - f(x_1)}{x_2 - x_1}",
+            font_size=27
+        ).next_to(Proof, DOWN).align_to(Proof.get_left(), LEFT)
+        
+        TempInProofTrans_2 = MathTex(
+            r"& \Leftarrow\quad  \frac{f(x_2) - f(x_1)}{x_2 - x_1} = \frac{f(x_2) - f(x_1)}{x_2 - x_1}",
+            font_size=27
+        ).next_to(Proof, DOWN).align_to(Proof.get_left(), LEFT)
+        
+        self.play(Write(TempInProofOri))
+        self.play(Transform(TempInProofOri, TempInProofTrans_1))
+        self.wait()
+        self.play(Transform(TempInProofOri, TempInProofTrans_2))
+        
+        QED = MathTex(r"& \text{另一边同理}\quad \text{证毕.}", tex_environment="align*", font_size=27).next_to(TempInProofOri, RIGHT)
+        
+        self.play(Write(QED))
+                
+        self.wait()
+        # 淡出所有对象
+        self.play(*[FadeOut(obj) for obj in self.mobjects])
+                
+    # def DefineConvexFunctionGraph(self):
+        
+    # def ContinuityOfConvexFunc(self):
+        
         
         
     def MyLogo(self):
